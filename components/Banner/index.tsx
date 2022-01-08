16c9e3e1ec3a6e600e6838c00a8a5a41c0e352f6
Fardin <fakurian.fardin@gmail.com>
2022-01-08 20:24:54 +0330

    Add Banner component

---
+++ components/Banner/index.tsx
@@ -0,0 +1,24 @@
+import { FunctionComponent } from "react";
+import styles from "./Banner.module.css";
+
+interface BannerProps {
+  buttonText: string;
+  handleClickOnButton: () => void;
+}
+
+const Banner: FunctionComponent<BannerProps> = ({ buttonText, handleClickOnButton }) => {
+  return (
+    <section className={styles.container}>
+      <h1 className={styles.title}>
+        <span className={styles.title1}>Coffee</span>
+        <span className={styles.title2}>Discovery</span>
+      </h1>
+      <p className={styles.subTitle}>Discover your local coffee shop!</p>
+      <button className={styles.button} onClick={handleClickOnButton}>
+        {buttonText}
+      </button>
+    </section>
+  );
+};
+
+export default Banner;
