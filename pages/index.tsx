34d070a16c8b433adccf5affd60882c0243fcf5d
Fardin <fakurian.fardin@gmail.com>
2022-01-09 21:38:59 +0330

    Add Card to the Home Page
    Fix next.config.json and add some fake data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Card from "../components/Card";
 import Banner from "../components/Banner";
 import styles from "../styles/Home.module.css";
+import coffeeStores from "../data/coffee-store.json";
 
 const Home: NextPage = () => {
   const handleClickOnBannerButton = () => console.log("clicked");
@@ -19,6 +21,16 @@ const Home: NextPage = () => {
         <Banner buttonText="View stores nearby" handleClickOnButton={handleClickOnBannerButton} />
         <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" alt="hero-image" width={700} height={400} />
+        </div>
+        <div className={styles.cardLayout}>
+          {coffeeStores.map(coffeeStore => (
+            <Card
+              key={coffeeStore.id}
+              name={coffeeStore.name}
+              imgUrl={coffeeStore.imgUrl}
+              href={`/coffee-store/${coffeeStore.id}`}
+            />
+          ))}
         </div>
       </main>
     </div>
