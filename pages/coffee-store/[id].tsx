58c5a39196b3838bc9c46ad589eb6d1e17452532
Fardin <fakurian.fardin@gmail.com>
2022-01-12 11:29:16 +0330

    Update coffee-store page

--- pages/coffee-store/[id].tsx
+++ pages/coffee-store/[id].tsx
@@ -1,10 +1,12 @@
 import { InferGetStaticPropsType, GetStaticPaths, NextPage, GetStaticProps } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { ParsedUrlQuery } from "querystring";
 import coffeeStoresData from "../../data/coffee-store.json";
-
+import cls from "classnames";
+import styles from "../../styles/coffee-store.module.css";
 interface IParams extends ParsedUrlQuery {
   id: string;
 }
@@ -32,8 +34,8 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 const CoffeeStore: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ coffeeStore }) => {
   const router = useRouter();
-  console.log({ coffeeStore });
 
+  const handleUpvote = () => console.log;
   if (router.isFallback) {
     return <div>Loading ...</div>;
   }
@@ -41,15 +43,40 @@ const CoffeeStore: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
     return <div>Nothing found for this route</div>;
   }
   return (
-    <div>
+    <div className={styles.layout}>
       <Head>
         <title>{coffeeStore.name}</title>{" "}
       </Head>
-      <Link href="/">
-        <a>Back to Home</a>
-      </Link>
-      <p>{coffeeStore.address}</p>
-      <p>{coffeeStore.name}</p>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">
+              <a>Back to Home</a>
+            </Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{coffeeStore.name}</h1>
+          </div>
+          <Image src={coffeeStore.imgUrl} alt={coffeeStore.name} width={600} height={360} className={styles.storeImg} />
+        </div>
+        <div className={cls("glass", styles.col2)}>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/places.svg" alt="" width={24} height={24} />
+            <p className={styles.text}>{coffeeStore.address}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/nearMe.svg" alt="" width={24} height={24} />
+            <p className={styles.text}>{coffeeStore.neighbourhood}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/star.svg" alt="" width={24} height={24} />
+            <p className={styles.text}>1</p>
+          </div>
+          <button className={styles.upvoteButton} onClick={handleUpvote}>
+            Up Vote!
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
