445a0f8e7dff78d6a2fcd5c79c2f7a86c7b94378
Fardin <fakurian.fardin@gmail.com>
2022-01-17 11:24:02 +0330

    Add unsplashApi to get some coffee-shop photos

--- lib/coffee-stores.ts
+++ lib/coffee-stores.ts
@@ -1,6 +1,9 @@
+import { createApi } from "unsplash-js";
+
 export interface CoffeeStore {
   fsq_id: number;
   name: string;
+  imgUrl?: string;
   location: {
     address: string;
     neighborhood?: string[];
@@ -10,8 +13,20 @@ interface CoffeeStores {
   results: CoffeeStore[];
 }
 
+const unsplashApi = createApi({
+  accessKey: process.env.UNSPLASH_ACCESS_KEY || "",
+});
+
 const getCoffeeStoresUrl = (latlng: string, query: string, limit?: number) => {
   return `https://api.foursquare.com/v3/places/nearby?ll=${latlng}&query=${query}&limit=${limit || 7}`;
+};
+const getCoffeeStoresPhotos = async () => {
+  const photos = await unsplashApi.search.getPhotos({
+    query: "coffee shop",
+    perPage: 10,
+  });
+  const results = photos.response?.results;
+  return results?.map(res => res.urls["small"]);
 };
 
 export const fetchCoffeeStores = async () => {
@@ -23,8 +38,11 @@ export const fetchCoffeeStores = async () => {
       Authorization: process.env.FOURSQUARE_API_KEY || "",
     },
   };
-
+  const photos = await getCoffeeStoresPhotos();
   const response = await fetch(url, options);
   const data: CoffeeStores = await response.json();
-  return data.results;
+  return data.results.map((res, idx) => ({
+    ...res,
+    imgUrl: photos ? photos[idx] : "",
+  }));
 };
