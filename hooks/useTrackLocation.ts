d7d8a9dd17565b45a7bde0196f594df576ac8108
Fardin <fakurian.fardin@gmail.com>
2022-01-20 18:29:39 +0330

    Add loading state to the useTrackLocation hook

--- hooks/useTrackLocation.ts
+++ hooks/useTrackLocation.ts
@@ -2,18 +2,23 @@ import { useState } from "react";
 
 const useTrackLocation = () => {
   const [locationErrorMsg, setLocationErrorMsg] = useState("");
+  const [isFindingLocation, setIsFindingLocation] = useState(false);
+
   const [latlng, setLatlng] = useState("");
   const success = (position: GeolocationPosition) => {
     const { latitude, longitude } = position.coords;
     setLatlng(`${latitude},${longitude}`);
     setLocationErrorMsg("");
+    setIsFindingLocation(false);
   };
 
   const error = () => {
     setLocationErrorMsg("Unable to retrieve your location");
+    setIsFindingLocation(false);
   };
 
   const handleTrackLocation = () => {
+    setIsFindingLocation(true);
     if (!navigator.geolocation) {
       setLocationErrorMsg("Gelocation is not supported by your browser!");
     } else {
@@ -24,6 +29,7 @@ const useTrackLocation = () => {
   return {
     latlng,
     locationErrorMsg,
+    isFindingLocation,
     handleTrackLocation,
   };
 };
