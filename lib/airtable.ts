28f7ccd9899f67fa0e848627587dfedfaa0d4f7c
Fardin <fakurian.fardin@gmail.com>
2022-03-20 15:21:53 +0330

    Fix type

--- lib/airtable.ts
+++ lib/airtable.ts
@@ -1,11 +1,18 @@
 import Airtable, { FieldSet, Records } from "airtable";
+import { CoffeeStore } from "./coffee-stores";
 
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_KEY || "");
 
 export const airTable = base("coffee-stores");
 
-export const getMinifiedRecords = (records: Records<FieldSet>) => {
-  return records.map(record => record.fields);
+interface AirTableCoffeeStore extends CoffeeStore {
+  recordId: string;
+}
+export const getMinifiedRecords = (records: Records<FieldSet>): AirTableCoffeeStore[] => {
+  return records.map(record => ({
+    recordId: record.id,
+    ...(record.fields as unknown as CoffeeStore),
+  }));
 };
 
 export const findRecordByFilter = async (id: number | string) => {
